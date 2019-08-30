d2c1fc3cc48abb36ab04239723c91e404886e157
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-30 13:59:25 +0100

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
+require_relative './customer.rb'
+require_relative './meal.rb'
+
 class Waiter
+
+    @@all = []
+
+    attr_accessor :name, :year
+
+    def initialize(name, year)
+        @name = name
+        @year = year
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total ,tip)
+    end
+
+    def meals
+        Meal.all.find_all{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        meals.max_by{|meal| meal.tip}.customer
+    end
 
 end
