9e0fe6c7eaabb99484835e1df0f10f98acf4871a
louiscl <51415142+louiscl@users.noreply.github.com>
2019-09-01 23:44:17 +0100

    Update customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
+require_relative "meal.rb"
+require_relative "waiter.rb"
+
 class Customer
   
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize (name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|m_ins| m_ins.customer == self}
+    end
+
+    def waiters
+        meals.map {|m| m.waiter}
+    end
+
 end
