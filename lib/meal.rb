38299656acd6764ff05ac0b76f74d817a4797239
github username <github email address>
2019-09-04 06:00:27 +0100

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,27 @@
-class Meal
+require 'pry'
 
+class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @Wcustomer = customer
+        @total = total 
+        @tip = tip
+        @@all << self
 end
+
+    def self.all
+        @all
+    end
+
+    def Meal
+        puts "hi"
+    end
+end
+
+
+binding.pry
+
